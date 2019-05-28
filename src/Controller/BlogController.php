b7e59ac21a0b68f874b1e1652642548c9fd3d814
ovy4you <ovy_4_you@yahoo.com>
2019-05-28 09:11:21 +0300

    commit

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,35 +2,77 @@
 
 namespace App\Controller;
 
+use App\Entity\BlogPost;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Serializer;
 
 /**
  * @Route("/blog")
  */
 class BlogController extends AbstractController
 {
+
     /**
-     * @Route("/", name="blog_list")
+     * @Route("/add", name="blog_add", methods={"POST"})
      */
-    public function list()
+    public function add(Request $request, EntityManagerInterface $em)
     {
+        /** @var Serializer $serializer */
+        $serializer = $this->get('serializer');
+
+        $blogPost = $serializer->deserialize($request->getContent(), BlogPost::class, 'json');
+
+        $em->persist($blogPost);
+        $em->flush();
+        return $this->json($blogPost);
+    }
+
+
+    /**
+     * @Route("/{page}", name="blog_list",defaults={"page":1},requirements={"page"="\d+"})
+     */
+    public function list($page, Request $request, EntityManagerInterface $em)
+    {
+        $blogPostRepository = $em->getRepository(BlogPost::class);
+        $items = $blogPostRepository->findAll();
+        return new JsonResponse(
+            ['page' => $page,
+                'data' => array_map(function (BlogPost $item) {
+                    return $this->generateUrl('blog_by_id', ['id' => $item->getId()]);
+                }, $items)
+            ]);
+    }
+
+    /**
+     * @Route("/post/{id}", name="blog_by_id",requirements={"id"="\d+"},methods={"GET"})
+     */
+    public function post(BlogPost $post)
+    {
+        return $this->json($post);
 
     }
 
     /**
-     * @Route("/{id}", name="blog_by_id")
+     * @Route("/{slug}", name="blog_by_slug")
      */
-    public function post()
+    public function postBySlug(BlogPost $post)
     {
-
+        return $this->json($post);
     }
 
     /**
-     * @Route("/{id}", name="blog_by_id")
+     * @Route("/post/{id}", name="blog_delete",requirements={"id"="\d+"},methods={"DELETE"})
      */
-    public function postBySlug()
+    public function delete(BlogPost $post,EntityManagerInterface $em)
     {
+        $em->remove($post);
+        $em->flush();
+        return $this->json(null,Response::HTTP_NO_CONTENT);
 
     }
 
---
+++ src/DataFixtures/BlogPostFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\BlogPost;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class BlogPostFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+         $blogPost = new BlogPost();
+
+         $blogPost->setText('text')
+             ->setAuthor('voodi')
+             ->setSlug('voodiu-')
+             ->setTitle('tiotle')
+             ->setPublished(new \DateTime());
+
+         $manager->persist($blogPost);
+
+        $manager->flush();
+    }
+}
